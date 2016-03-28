0195c46dab53bdb5c8ece585980955320133125f
Nicholas Duvieilh <nickyduvieilh@gmail.com>
2016-03-28 15:19:19 -0500

    First commit

---
+++ app/src/main/java/com/thenicky/fleettracker/DBHelper.java
@@ -0,0 +1,107 @@
+package com.thenicky.fleettracker;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "tracker.db";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "";
+
+        db.execSQL(
+                "create table vehicles " +
+                        "(id integer primary key, vehicle_id ,phone text,email text, street text,place text)"
+        );
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+        db.execSQL("DROP TABLE IF EXISTS contacts");
+        onCreate(db);
+    }
+
+    public boolean insertContact(String name, String phone, String email, String street, String place) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("phone", phone);
+        contentValues.put("email", email);
+        contentValues.put("street", street);
+        contentValues.put("place", place);
+        db.insert("contacts", null, contentValues);
+        return true;
+    }
+
+    public Cursor getData(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = db.rawQuery("select * from contacts where id=" + id + "", null);
+        return res;
+    }
+
+//    public int numberOfRows() {
+//        SQLiteDatabase db = this.getReadableDatabase();
+//        int numRows = (int) DatabaseUtils.queryNumEntries(db, CONTACTS_TABLE_NAME);
+//        return numRows;
+//    }
+
+    public boolean updateContact(Integer id, String name, String phone, String email, String street, String place) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("phone", phone);
+        contentValues.put("email", email);
+        contentValues.put("street", street);
+        contentValues.put("place", place);
+        db.update("contacts", contentValues, "id = ? ", new String[]{Integer.toString(id)});
+        return true;
+    }
+
+    public Integer deleteContact(Integer id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("contacts",
+                "id = ? ",
+                new String[]{Integer.toString(id)});
+    }
+
+//    public ArrayList<String> getAllCotacts() {
+//        ArrayList<String> array_list = new ArrayList<String>();
+//
+//        SQLiteDatabase db = this.getReadableDatabase();
+//        Cursor res = db.rawQuery("select * from contacts", null);
+//        res.moveToFirst();
+//
+//        while (res.isAfterLast() == false) {
+//            array_list.add(res.getString(res.getColumnIndex(CONTACTS_COLUMN_NAME)));
+//            res.moveToNext();
+//        }
+//        return array_list;
+//    }
+}
